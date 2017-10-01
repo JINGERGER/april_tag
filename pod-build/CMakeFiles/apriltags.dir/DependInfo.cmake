57a256ac0fbb3abf518bcf4137324fc114b7acc8
JINGERGER <westlake601@gmail.com>
2017-09-30 17:16:36 -0700

    apriltag_testing

---
+++ pod-build/CMakeFiles/apriltags.dir/DependInfo.cmake
@@ -0,0 +1,40 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ros/apriltags/src/Edge.cc" "/home/ros/apriltags/pod-build/CMakeFiles/apriltags.dir/src/Edge.cc.o"
+  "/home/ros/apriltags/src/FloatImage.cc" "/home/ros/apriltags/pod-build/CMakeFiles/apriltags.dir/src/FloatImage.cc.o"
+  "/home/ros/apriltags/src/GLine2D.cc" "/home/ros/apriltags/pod-build/CMakeFiles/apriltags.dir/src/GLine2D.cc.o"
+  "/home/ros/apriltags/src/GLineSegment2D.cc" "/home/ros/apriltags/pod-build/CMakeFiles/apriltags.dir/src/GLineSegment2D.cc.o"
+  "/home/ros/apriltags/src/Gaussian.cc" "/home/ros/apriltags/pod-build/CMakeFiles/apriltags.dir/src/Gaussian.cc.o"
+  "/home/ros/apriltags/src/GrayModel.cc" "/home/ros/apriltags/pod-build/CMakeFiles/apriltags.dir/src/GrayModel.cc.o"
+  "/home/ros/apriltags/src/Homography33.cc" "/home/ros/apriltags/pod-build/CMakeFiles/apriltags.dir/src/Homography33.cc.o"
+  "/home/ros/apriltags/src/MathUtil.cc" "/home/ros/apriltags/pod-build/CMakeFiles/apriltags.dir/src/MathUtil.cc.o"
+  "/home/ros/apriltags/src/Quad.cc" "/home/ros/apriltags/pod-build/CMakeFiles/apriltags.dir/src/Quad.cc.o"
+  "/home/ros/apriltags/src/Segment.cc" "/home/ros/apriltags/pod-build/CMakeFiles/apriltags.dir/src/Segment.cc.o"
+  "/home/ros/apriltags/src/TagDetection.cc" "/home/ros/apriltags/pod-build/CMakeFiles/apriltags.dir/src/TagDetection.cc.o"
+  "/home/ros/apriltags/src/TagDetector.cc" "/home/ros/apriltags/pod-build/CMakeFiles/apriltags.dir/src/TagDetector.cc.o"
+  "/home/ros/apriltags/src/TagFamily.cc" "/home/ros/apriltags/pod-build/CMakeFiles/apriltags.dir/src/TagFamily.cc.o"
+  "/home/ros/apriltags/src/UnionFindSimple.cc" "/home/ros/apriltags/pod-build/CMakeFiles/apriltags.dir/src/UnionFindSimple.cc.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "include"
+  "../build/include"
+  "../AprilTags"
+  "../."
+  "/opt/local/include"
+  "/usr/include/opencv"
+  "/usr/include/eigen3"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
